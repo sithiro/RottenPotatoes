779573861e07af6e415cd76cbd09710821859c11
Bill Sithiro <sithiro@gmail.com>
2013-02-05 00:49:24 +0200

    hw2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,17 +7,28 @@ class MoviesController < ApplicationController
   end
 
   def index
-	@hilite1 = ""
-	@hilite2 = ""	
-	sort = params[:sort]
-	if sort == "alpha"
+	@all_ratings = ['G','PG','PG-13','R']  
+	@hilite1 = @hilite2 = ""
+	items = Movie.all	
+	@params = params
+	@sort = params[:sort]
+	@ratings = params[:ratings]	
+	if @ratings == nil			
+		@ratings = Hash.new			
+	else
+		items = Movie.where(:rating => @ratings.keys)
+		@ratings_link = ""
+		@ratings.keys.each do |rating|
+			@ratings_link = @ratings_link + "ratings%5B#{rating}%5D=1&"
+		end
+	end
+	
+	if @sort == "alpha"
 		@hilite1 = "hilite"
-		items = Movie.all.sort{|p1,p2| p1.title <=> p2.title}
-	elsif sort == "date"	
+		items = items.sort{|p1,p2| p1.title <=> p2.title}
+	elsif @sort == "date"	
 		@hilite2 = "hilite"
-		items = Movie.all.sort{|p1,p2| p1.release_date <=> p2.release_date} 		
-	elsif		
-		items = Movie.all
+		items = items.sort{|p1,p2| p1.release_date <=> p2.release_date} 		
 	end
 		
 	@movies = items
