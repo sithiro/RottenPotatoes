c5981a1c812c7863100c3733573d90195f0991a7
Bill Sithiro <sithiro@gmail.com>
2013-02-05 12:15:41 +0200

    hw2-3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,37 +1,42 @@
 class MoviesController < ApplicationController
-
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
+  def handle_state
 	@all_ratings = ['G','PG','PG-13','R']  
 	@hilite1 = @hilite2 = ""
-	items = Movie.all	
+	@items = Movie.all	
 	@params = params
 	@sort = params[:sort]
-	@ratings = params[:ratings]	
+	@ratings = params[:ratings]
 	if @ratings == nil			
 		@ratings = Hash.new			
 	else
-		items = Movie.where(:rating => @ratings.keys)
+		@items = Movie.where(:rating => @ratings.keys)
 		@ratings_link = ""
 		@ratings.keys.each do |rating|
 			@ratings_link = @ratings_link + "ratings%5B#{rating}%5D=1&"
 		end
-	end
+	end	
+	@none_selected = @ratings.length == 0	
+  end
+  
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+	handle_state
+  end
+  
+  def index
+	handle_state
 	
 	if @sort == "alpha"
 		@hilite1 = "hilite"
-		items = items.sort{|p1,p2| p1.title <=> p2.title}
+		@items = @items.sort{|p1,p2| p1.title <=> p2.title}
 	elsif @sort == "date"	
 		@hilite2 = "hilite"
-		items = items.sort{|p1,p2| p1.release_date <=> p2.release_date} 		
+		@items = @items.sort{|p1,p2| p1.release_date <=> p2.release_date} 		
 	end
 		
-	@movies = items
+	@movies = @items
   end
   
   def new
